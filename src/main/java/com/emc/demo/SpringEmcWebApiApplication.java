fdb7af7a4cc880994de7d37f95640dc6132f699d
GCA\jgonz296 <gonzojuan@hotmail.com>
2022-11-09 12:51:12 +0100

    no message

---
+++ src/main/java/com/emc/demo/SpringEmcWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.emc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmcWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmcWebApiApplication.class, args);
+	}
+
+}
