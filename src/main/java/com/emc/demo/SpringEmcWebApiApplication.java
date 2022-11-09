114c8a1e20a9208df36080c25eede807a92a2452
GCA\jgonz296 <gonzojuan@hotmail.com>
2022-11-09 13:01:14 +0100

    First commit (aunque ya hay otros pq ya lo había hecho)

--- src/main/java/com/emc/demo/SpringEmcWebApiApplication.java
+++ src/main/java/com/emc/demo/SpringEmcWebApiApplication.java
@@ -2,7 +2,7 @@ package com.emc.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Clase que arranca el programa
 @SpringBootApplication
 public class SpringEmcWebApiApplication {
 
---
+++ src/test/java/com/emc/demo/SpringEmcWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmcWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
