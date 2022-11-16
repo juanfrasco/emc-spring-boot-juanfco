133228844f87f268c1dc1580418c974195a9adf3
GCA\jgonz296 <gonzojuan@hotmail.com>
2022-11-16 21:50:56 +0100

    - Afegit Dockerfile i Start.bat

---
+++ Start.bat
@@ -0,0 +1,4 @@
+set MYSQL_URL=jdbc:mysql://eu-cdbr-west-01.cleardb.com:3306/heroku_0cfc546621a0fc3
+set USERNAME=b4867549279eb6
+set PASSWORD=c2e5e0e2
+start java -jar target/spring-emc-web-api-0.0.1-SNAPSHOT.jar
